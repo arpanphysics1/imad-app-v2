0fdac11d0bb14ca3293dd27bc49bd7ca5e607d6f
arpanphysics1 <arpanphysics1@gmail.com>
2017-05-14 16:53:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,17 @@
-// Include required libraries
-var Bot = require('node-telegram-bot');
 
-// Initialize and start Telegram BOT (insert your real token)
-var bot = new Bot({
-  token: '0123456789abcdef0123456789abcdef0123456789ab'
-});
-
-// Attach event on every received message 
-bot.on('message', function (message) {
-  echo(message);
-});
-
-// Start the bot
-bot.start();
-console.log("BOT ready!");
-
-// Function that handles a new message
-function echo(message) {
-  
-  bot.sendMessage({
-	  chat_id: message.chat.id,
-	  text: 'User ID ' + message.from.id + ' says "' + message.text + '"',
-  });
+//console.log('Loaded!');
+{
+  "name": "imad-2016-base",
+  "version": "0.1.0",
+  "description": "IMAD 2016 course app",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.14.0",
+    "morgan": "^1.7.0"
+  }
 }
